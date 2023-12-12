254d5d3814de398fa19c6de9c0ac7bfe03e1f011
DPG\Yomala <yomala@dpinfotech.lk>
2023-12-12 05:51:46 +0530

    Add project files.

---
+++ ChatApplication/Hub/ChatHub.cs
@@ -0,0 +1,48 @@
+﻿namespace ChatApplication.Hub
+{
+    using Microsoft.AspNetCore.SignalR;
+    public class ChatHub : Hub
+    {
+        private readonly IDictionary<string, UserRoomConnection> _connection;
+        public ChatHub(IDictionary<string, UserRoomConnection> connection)
+        {
+            _connection = connection;
+        }
+        public async Task JoinRoom(UserRoomConnection userConnection)
+        {
+
+            await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room!);
+            _connection[Context.ConnectionId] = userConnection;
+
+            await Clients.Group(userConnection.Room!)
+                .SendAsync("ReveiveMessage", "Lets Program Bot", $"{userConnection.User} has joind the group");
+            await SendConnectedUsers(userConnection.Room!);
+        }
+
+        public async Task SendMessage(string message)
+        {
+            if (_connection.TryGetValue(Context.ConnectionId, out UserRoomConnection userRoomConnection))
+            {
+                await Clients.Group(userRoomConnection.Room!)
+                        .SendAsync("ReveiveMessage", userRoomConnection.User, message, DateTime.Now);
+            }
+        }
+
+        public Task SendConnectedUsers(string room)
+        {
+            var users = _connection.Values.Where(u => u.Room == room).Select(u => u.User);
+            return Clients.Group(room).SendAsync("ConnectedUser", users);
+        }
+
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            if (!_connection.TryGetValue(Context.ConnectionId, out UserRoomConnection userConnection))
+                return base.OnDisconnectedAsync(exception);
+
+            Clients.Group(userConnection.Room!)
+                .SendAsync("ReveiveMessage", "Lets Program Bot", $"{userConnection.User} has left the group");
+            SendConnectedUsers(userConnection.Room!);
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
---
+++ ChatApplication/Program.cs
@@ -0,0 +1,35 @@
+using ChatApplication;
+using ChatApplication.Hub;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
+builder.Services.AddSingleton<IDictionary<string, UserRoomConnection>>(opt =>
+new Dictionary<string, UserRoomConnection>());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(); 
+}
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapHub<ChatHub>("/Chat");
+});
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
